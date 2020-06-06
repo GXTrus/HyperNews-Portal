4b69bac1430e689e3ffac867b6fcb592d219c13f
Alexandr Pahomov <gxtrus@gmail.com>
2020-06-06 14:26:17 +0300

    Stage #4: Creating new news
    Description
    
    You store your news data in the JSON file. Every time you want to add news, you have to edit this file, which is quite inconvenient. That's why you need to add the possibility to create news through your web interface.
    
    Objectives
    
    Create a new page to add news using a form. This form should send data to the /news/create/ address with the POST method to create a new record with two parameters, title and text. When the request is completed, the response should redirect you back to the main page.
    
    If you start the application on your computer with the python manage.py runserver command, the page for creating news should be available at the localhost:8000/news/create/ address.
    
    The page should also contain a link to the main page link with target="_blank" href="/news/".
    
    The link in your storage should be a random number, but remember that it's unique for every news. Choose the current time for the created field, and do not forget to convert it to a string before saving to JSON.

--- HyperNews Portal/task/hypernews/news/views.py
+++ HyperNews Portal/task/hypernews/news/views.py
@@ -1,13 +1,18 @@
 import datetime
 import json
-
+import random
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 
 
 # Create your views here.
+def get_random(used):
+    a = random.randint(1, 999999)
+    if a in used:
+        a = get_random(used)
+    return a
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
@@ -42,8 +47,40 @@ class PostView(View):
         response = ''
         for post_ in news:
             if str(post_['link']) == post:
-                response = f'<h2>{post_["title"]}</h2>\n'
+                response += f'<h2>{post_["title"]}</h2>\n'
                 response += f'<p>{post_["created"]}</p>\n'
                 response += f'<p>{post_["text"]}</p>\n'
                 response += '<div><a href="/news/">News</a></div>'
         return HttpResponse(response)
+
+
+class CreatePostView(View):
+    def get(self, request, *args, **kwargs):
+        # response = '<form method="post">{% csrf_token %}'
+        # response += '<p>Title:</p>'
+        # response += '<input type="text" name="title">'
+        # response += '<p>Text:</p>'
+        # response += '<input type="text" name="text">'
+        # response += '<p><button type="submit">Submit</button></p>'
+        # response += '</form>'
+        # return HttpResponse(response)
+        return render(request, 'news/create.html')
+
+    def post(self, request, *args, **kwargs):
+        with open(settings.NEWS_JSON_PATH, "r") as json_news:
+            news = json.load(json_news)
+        used_links = []
+        for post in news:
+            used_links.append(post['link'])
+        link = get_random(used_links)
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_post_dict = { 'created': time, "text": text, "title": title, 'link': link}
+        news.append(new_post_dict)
+        with open("news.json", "w") as json_file:
+            json.dump(news, json_file)
+        # return HttpResponse(response)
+
+        # return HttpResponse(line_of_cars.processing())
+        return redirect('/news')
